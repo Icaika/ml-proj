056d7b9c29cf181e73ecd0ce873a41195ed9b6be
Nikunj Gupta <120092624+Icaika@users.noreply.github.com>
2025-04-23 19:22:53 +0530

    Added generic stuff

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,21 +1,18 @@
 import sys
+from logger import logging
 
 def error_message(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     filename = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name {0} @ line number {1}, error message [{2}]".format(filename, exc_tb.tb_lineno, str(error))
-    
-    return error_message
+    error_msg = "Error occurred in python script name {0} @ line number {1}, error message [{2}]".format(
+        filename, exc_tb.tb_lineno, str(error)
+    )
+    return error_msg
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
-        self.error_message = error_message(error_message, error_detail = error_detail)
-    
+    def __init__(self, msg, error_detail: sys):
+        super().__init__(msg)
+        self.error_message = error_message(msg, error_detail)
+
     def __str__(self):
         return self.error_message
-    
-    
-        
-        
-        
